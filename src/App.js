cca7d266637ceb113908e1f4d6b4955a7edfeda4
Gamerman2001 <Tro12185@gmail.com>
2020-02-15 15:31:24 -0500

    setup all API calls into useEffect, ready to pass down into other components

--- src/App.js
+++ src/App.js
@@ -1,37 +1,76 @@
-import React, {useState, useEffect} from 'react';
-import logo from './logo.svg';
-import './App.scss';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import Login from "./containers/Login/Login";
+import "./App.scss";
 
 function App() {
-  const [room, setRoom] = useState({})
+  const [room, setRoom] = useState({
+    testing: "yes",
+    roomId: null,
+    positions: null,
+    students: null,
+    // desks: null,
+    // dates: null,
+    dailyData: null
+  });
 
   useEffect(() => {
     //we will set up objects for our API calls in here
-    setRoom({
-      roomId: null,
-      positions: null,
-      students: null,
-      desks: null,
-      dates: null,
-      dailyData: null
-    })
-  },[]);
+    const fetchClassroom = async () => {
+      const roomData = await fetch("http://localhost:5000/roomdata")
+        .then(response => {
+          return response.json();
+        })
+        .then(myJson => {
+          return myJson.roomData;
+        });
+      const dailyData = await fetch("http://localhost:5000/dailydata")
+        .then(response => {
+          return response.json();
+        })
+        .then(myJson => {
+          return myJson.dailyData;
+        });
+      const studentData = await fetch("http://localhost:5000/studentdata")
+        .then(response => {
+          return response.json();
+        })
+        .then(myJson => {
+          return myJson.studentData;
+        });
+      const deskData = await fetch("http://localhost:5000/deskdata")
+      .then(response => {
+        return response.json();
+      })
+      .then(myJson => {
+        return myJson.deskData
+      });
+
+      setRoom({
+        ...room,
+        positions: roomData[0].positions,
+        roomId: roomData[0].roomId,
+        dailyData: dailyData,
+        students: studentData,
+        desk: deskData
+      });
+    };
+    fetchClassroom();
+    //End of fetching of resources
+    // setRoom({
+    //   roomId: null,
+    //   positions: null,
+    //   students: null,
+    //   desks: null,
+    //   dates: null,
+    //   dailyData: null
+    // });
+  }, []);
+
+  console.log(room);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Login room={room} />
     </div>
   );
 }
---
+++ src/containers/Login/Login.js
@@ -0,0 +1,12 @@
+import React, {useEffect}from 'react'
+
+export default function Login(props) {
+  useEffect(()=> {
+    console.log(props)
+  })
+  return (
+    <div>
+      hello!
+    </div>
+  )
+}
