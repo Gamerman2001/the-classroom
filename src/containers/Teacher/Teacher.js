0575711d05aca65fba5d33acc93462e1ed0039c8
Gamerman2001 <Tro12185@gmail.com>
2020-02-16 12:43:48 -0500

    adding desk component to teacher

--- src/containers/Teacher/Teacher.js
+++ src/containers/Teacher/Teacher.js
@@ -1,5 +1,6 @@
 //Teacher component will show room #, 8 desk layout, sign out, present & absence 
 import React, {useEffect} from 'react'
+import Desk from '../Desk/Desk'
 
 export default function Teacher(props) {
   const {dailyData, desk, positions, roomId} = props.room
