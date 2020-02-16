9cec47bae12b5a818ca4167ade1e77c0a27086b9
Gamerman2001 <Tro12185@gmail.com>
2020-02-16 14:34:43 -0500

    added day filter for Teacher component

--- src/containers/Desk/Desk.js
+++ src/containers/Desk/Desk.js
@@ -1,1 +1,13 @@
 //will show data for a desk
+import React, {useState}from 'react'
+
+export default function Desk(props) {
+  const {user, dailyData, Desk ,roomId, pos} = props
+
+  return (
+    <div>
+      {/* {user}
+      {dailyData} */}
+    </div>
+  )
+}
--- src/containers/Teacher/Teacher.js
+++ src/containers/Teacher/Teacher.js
@@ -1,17 +1,48 @@
-//Teacher component will show room #, 8 desk layout, sign out, present & absence 
-import React, {useEffect} from 'react'
-import Desk from '../Desk/Desk'
+//Teacher component will show room #, 8 desk layout, sign out, present & absence
+import React, { useEffect, useState } from "react";
+import Desk from "../Desk/Desk";
 
 export default function Teacher(props) {
-  const {dailyData, desk, positions, roomId} = props.room
-  useEffect (()=>{
-    console.log(dailyData, desk, positions, roomId, 'this is coming from Teacher')
-  }) 
+  const { dailyData, desk, positions, roomId } = props.room;
+  useEffect(() => {
+    console.log(dailyData);
+  });
+  const [day, setDay] = useState("Please Select a Date");
+  const changeDay = day => {
+    setDay(day.date);
+  };
+  const grabDays = dailyData.map(day => {
+    return (
+      <a
+        key={day.date}
+        onClick={() => {
+          changeDay(day);
+        }}
+        href="# "
+      >
+        {day.date + "  "}
+      </a>
+    );
+  });
 
-  
   return (
     <div>
-      Teacher Component
+      <div>{grabDays}</div>
+
+      <div>CURRENT DAY: {day}</div>
+      {positions.map((pos, index) => {
+        return (
+          <Desk
+            key={index}
+            user="Teacher"
+            day={day}
+            dailyData={dailyData}
+            desk={desk}
+            roomId={roomId}
+            pos={pos}
+          />
+        );
+      })}
     </div>
-  )
+  );
 }
